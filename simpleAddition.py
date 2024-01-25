7192983e65bcc5aafcc73c002c3a0f8e5e44929b
Duba2023 <157641452+Duba2023@users.noreply.github.com>
2024-01-25 16:03:23 +0100

    Create simpleAddition.py
    A simple Python script that can collect input from the user and perform a simple addition

---
+++ simpleAddition.py
@@ -0,0 +1,8 @@
+num1 = input("Please enter the first number")
+num = f"\nEnter the second nmuber and i will display the result for you"
+print(num)
+num2 = input()
+num1 = int(num1)
+num2 = int(num2)
+Result = num1 + num2
+print(Result)
